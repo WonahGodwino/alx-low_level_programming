2a7a593a5f60ced9fd199e9da06283297940471c
WonahGodwino <atamswgo1@gmail.com>
2022-04-12 15:50:56 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
+ *
  *main-main block
  *Description:print -putchar in a new line
- *Return: this program returns zero
+ *Return:this program returns zero
  */
 int main(void)
 {	
