062bd7df3437b4ad74f42c8983baba5b5e1a6e38
WonahGodwino <atamswgo1@gmail.com>
2022-04-12 15:05:25 -0700

    o-putchar.c
    git add .
    git add .
    cd alx-low_level_programming/0x02-functions_nested_loops
     alx-low_level_programming/0x02-functions_nested_loops
    git commit -m o-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,31 +1,15 @@
 #ifndef _MAIN_H_
-
 #define _MAN_H_
-
-
-
 int _putchar(char c);
-
 void print_alphabet_x10(void);
-
 int _islower(int c);
-
 int _isalpha(int c);
-
 int print_sign(int n);
-
 int _abs(int);
-
 int print_last_digit(int);
-
 void jack_bouver(void);
-
 void times_table(void);
-
 int add(int, int);
-
 void print_to_98(int n);
-
-void print_times_table(int n)
-
+void print_times_table(int n);
 #endif /* _MAN_H_ */
