936d92dac699f9b0495e4aa04b483b23c17f2e2c
WonahGodwino <atamswgo1@gmail.com>
2022-04-12 14:39:55 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,31 @@
+#ifndef _MAIN_H_
+
+#define _MAN_H_
+
+
+
+int _putchar(char c);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bouver(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n)
+
+#endif /* _MAN_H_ */
