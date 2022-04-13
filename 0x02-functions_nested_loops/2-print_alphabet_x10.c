c4c03d4d31dcd70deb6d9923bf51c89a2c821c78
WonahGodwino <atamswgo1@gmail.com>
2022-04-13 10:32:17 -0700

    print alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * print_alphabet_x10 print alphabet 10 times
+ * print_alphabet_x10:Entry Point 
  * Return: void
  */
 void print_alphabet_x10(void)
 /*print_alphabeit_x10: Entry point*/
 {
 char c;
-int i = 10;
+int i = 0;
 while (i < 10)
 {
 c = 'a';
