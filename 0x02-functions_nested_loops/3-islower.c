feed8916ac729cae5aeff782a57c1ee37bbb606b
WonahGodwino <atamswgo1@gmail.com>
2022-04-13 16:08:20 -0700

    islower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,8 +5,7 @@
 *return: 1 if character is lowercase, 0 otherwise
 */
 int _islower(int c)
-/*_islower: Entry poin*/
+/*_islower:Entry poin*/
 {
-/*returns 1 if c is lowercase returns 0 otherwise*/
 return (c >= 97 && c <= 122);
 }
