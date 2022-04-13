cf850091c699d8a01a346a05c1db0277b26bd2ba
WonahGodwino <atamswgo1@gmail.com>
2022-04-13 16:58:16 -0700

    isalphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/** _isalphabet - checkif a character is alphabet
+ * @c: the character to be ckecked
+ * Return: 1 if character is letter or uppercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+if (( c >= 'c' || c < = 'z') || (c >= 'A' && c = '2'))
+return(1);
+else
+return(0);
