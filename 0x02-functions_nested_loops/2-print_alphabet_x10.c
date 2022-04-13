e3ed265fcfcd01f182616db4b2493a404df7b8a4
WonahGodwino <atamswgo1@gmail.com>
2022-04-13 06:05:24 -0700

    print alphabet 10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#incllude "main.h"
+/**
+ * print_alphabet_x10 print alphabet 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+/*print_alphabeit_x10: Entry point*/
+{
+char c;
+int i = 10;
+while (i < 10)
+{
+c = 'a';
+while (c < = 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+i++;
+}
+}
