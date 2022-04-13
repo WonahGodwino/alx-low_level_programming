14aca5e1906beab18ffe4ef26c2827f7fa4a6797
WonahGodwino <atamswgo1@gmail.com>
2022-04-13 11:51:03 -0700

    check lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,11 @@
 /**
  *_islower-check for lower case letter
  *@c:charcter to check the case
- * return:0 or 1
+ * return: 1 if character is lowercase 0 otherwise 
  */
-int _islower(int c)
-/* _islower: Entry point check ofr lower case letter*/
+init _islower(int c)
+/*_islower: Entry point checkr for lower case letter*/
 {
-/*returns 1 if c is lower case returns 0 otherwise*/
+/*returns 1 if c is lowercase returns 0 otherwise*/
 return (c >= 97 && c <= 122);
 }
