fe78240160b2fa665efb0245ec406e7376a1ab3a
WonahGodwino <atamswgo1@gmail.com>
2022-04-13 04:08:48 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *main - check the code
+ *function print alphabet
+ *Return: Always 0.
+ */
+void print_alphabet(void)
+/*declaration after main*/
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
