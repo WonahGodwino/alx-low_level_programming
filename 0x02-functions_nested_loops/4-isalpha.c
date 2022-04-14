259e7ba5eb04364abf50ab8b0e1e91fac0ce7b0a
WonahGodwino <atamswgo1@gmail.com>
2022-04-13 17:40:00 -0700

    isalphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
 #include "main.h"
-/** _isalphabet - checkif a character is alphabet
+/**_isalphabet - checkif a character is alphabet
  * @c: the character to be ckecked
  * Return: 1 if character is letter or uppercase, 0 otherwise
  */
 int _isalpha(int c)
 {
-return (( c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+return((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
 }
