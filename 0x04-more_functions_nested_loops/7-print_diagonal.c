906cc59c9ac7f30a2bd0e35819c6987fc4927dbe
WonahGodwino <atamswgo1@gmail.com>
2022-04-16 23:03:53 +0000

    I wish to go diagonal today

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,26 +1,33 @@
 #include "main.h"
 
 /**
- * print_diagonal - draws a diagonal line on the terminal
- * @n: number of times the character \ should be printed
+ * print_diagonal - Entry point
+ *@n: number of times to print '\' in diagonal
+ * Return: no return
  */
 void print_diagonal(int n)
 {
-	int i, j;
+	int i;
+	char k;
+	char s;
+	int j;
 
+	k = '\\';
+	s = ' ';
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < n; j++)
+		if (n > 1)
 		{
-			if (j == i)
+			for (j = 0; j < i; j++)
 			{
-			_putchar('\\');
+				_putchar(s);
 			}
-			else if (j < i)
-			{
-				_putchar(' ');
-			}
+			_putchar(k);
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
 }
