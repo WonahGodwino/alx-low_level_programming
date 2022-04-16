92d0744125d16dc4920babba6f9e3aa22e15c59b
WonahGodwino <atamswgo1@gmail.com>
2022-04-16 13:27:30 +0000

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,29 +9,29 @@ void jack_bauer(void)
 	int h2;
 	int m1;
 	int a = 9;
+
 	h2 = 0;
-	
-while (h2 <= 2)
+	while (h2 <= 2)
 	{
 		/* Restrain to 23hrs not 29*/
-		a = 3;
+	a = 3;
 	}
 	h1 = 0;
 	while (h1 <= a)
 	{
-		m2 = 0;
+	m2 = 0;
 		while (m2 <= 5)
 		{
-			m1 = 0;
-			while (m1 <= 9)
-			{
-				_putchar('0' + h2);
-				_putchar('0' + h1);
-				_putchar('0' + m2);
-				_putchar('0' + m1);
-				_putchar('\n');
-				m1++;
-			}
+		m1 = 0;
+		while (m1 <= 9)
+		{
+			_putchar('0' + h2);
+			_putchar('0' + h1);
+			_putchar('0' + m2);
+			_putchar('0' + m1);
+			_putchar('\n');
+			m1++;
+		}
 			m2++
 		}
 		h1++
