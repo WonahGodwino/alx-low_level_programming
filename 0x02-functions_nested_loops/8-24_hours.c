266718b380827504a80501e432a445933031d03e
WonahGodwino <atamswgo1@gmail.com>
2022-04-16 13:47:46 +0000

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,9 +2,7 @@
 
 /**
  * jack_bauer - print every minutes of the day
- *
- * Starting from 00:00 to 23:59 * @n: The number
- *
+ * @n: the number Starting from 00:00 to 23:59
  * Return: no return value
  */
 void jack_bauer(void)
@@ -12,7 +10,7 @@ void jack_bauer(void)
 	int hour = 0;
 	int minute = 0;
 
-	for (; hour < 24; hour++)
+	for (hour = 0; hour < 24; hour++)
 	{
 		for (minute = 0; minute < 60; minute++)
 			printf("%02d:%02d\n", hour, minute);
