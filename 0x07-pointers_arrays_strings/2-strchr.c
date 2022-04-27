836405bb051c4e9a9d25a5c0ccd9a289ea866b9e
WonahGodwino <atamswgo1@gmail.com>
2022-04-27 23:43:39 +0000

    search char in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - Function to search for char in string
+ * @s: input string
+ * @c: string to search for
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] >= 0 && s[i] != '\0')
+	{
+		if (s[i] == c)
+			{
+			return (&s[i]);
+			}
+		i++
+	}
+	return (0);
