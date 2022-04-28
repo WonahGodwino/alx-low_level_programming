d8f848176b7a01e34d88571cc8f30830b16765a8
WonahGodwino <atamswgo1@gmail.com>
2022-04-28 00:35:23 +0000

    search for char in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,17 +7,11 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	unsigned int i = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-			{
-			return (&s[i]);
-			}
-		else
-		{
-			i++;
-		}
-	}
-	return (0);
+	for (i=0; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	return ('\0');
+}
+
