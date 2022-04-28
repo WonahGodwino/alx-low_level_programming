cc33aec474281a3efb7fc1073b5769a526a95c58
WonahGodwino <atamswgo1@gmail.com>
2022-04-28 01:21:38 +0000

    search substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * 3-strspn.c - find the length of a substring from accept,
+ * @accept: substring.
+ * @s: string.
+ * Return: (0).
+ */
+unsigned int _strspn(char *s, char *accept);
+{
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring.
+ * @s: initial segment.
+ * @accept: accepted bytes.
+ * Return: the number of accepted bytes.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int c = 0;
+	int j;
+
+	while ((*s) != '\0')
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (*s == accept[j])
+			{
+				c++;
+				break;
+			}
+			else if (accept[j + 1] == '\0')
+				return (c);
+		}
+		s++;
+	}
+	return (c);
+}
