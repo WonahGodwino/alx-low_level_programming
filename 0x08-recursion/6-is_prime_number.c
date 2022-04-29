96618f0d7c3a99d1387234fda5f3635fcc934979
WonahGodwino <atamswgo1@gmail.com>
2022-04-29 22:20:37 +0000

    Prime Number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,33 +1,39 @@
 #include "main.h"
 /**
-* is_prime_number - Function to check for prime number
-* @num: number to be checked
-* Return: 1 or 0
+ * primeno - Second function
+ * @n: number to be checked
+ * @2: To check for prime
+ * Return: 1 or 0
 */
-int primeno(int n, int i)
-{ 
-    if (n / i == 1)
+int primeno(int n, 2)
+{
+	if ((n / 2) % 2 == 1)
 	{
-		return (1);
+	return (1);
 	}
 	else
 	{
-		return (0);
+	return (0);
 	}
 }
+/**
+ * is_prime_number - Functiond to check for prime number
+ * @n: number to be checked
+ * Return: 1 or 0
+ */
 int is_prime_number(int n)
 {
 	if (n == 1)
 	{
-		return (1);
+	return (1);
 	}
 	else
 	if (n % 2 == 0)
 	{
-		return (0);
+	return (0);
 	}
-       else
+	else
 	{
-         	return primeno(n, 2);
+	return (primeno(n, 2));
 	}
 }
