4603c37a4040c59af70daac803b3938af620868d
WonahGodwino <atamswgo1@gmail.com>
2022-04-29 17:28:22 +0000

    Length of string by recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 /**
  *_strlen_recursion - Recursion function to find the length of a string
  * @s: string to find its length by recursion
- * return: Always 0.
+ * Return: 0.
  */
 int _strlen_recursion(char *s)
 {
