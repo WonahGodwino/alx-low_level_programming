9ff4c9ba3a2bc6622248cdc05b05fefb7fb66d40
WonahGodwino <atamswgo1@gmail.com>
2022-04-29 01:47:29 +0000

    factorial using recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
- *_strlen_recursion - Recursive function to find the length of a string
+ *_strlen_recursion - Recursion function to find the length of a string
  * @s: string to find its length by recursion
  * return: Always 0.
  */
-
 int _strlen_recursion(char *s)
 {
-
 	if (*(s) != '\0')
 	{
 		return (1 + (_strlen_recursion(s + 1)));
