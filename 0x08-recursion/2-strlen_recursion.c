36a276fc50bfa657353e429f084cf6c4859a161f
WonahGodwino <atamswgo1@gmail.com>
2022-04-29 01:26:28 +0000

    Finding length of string using recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 /**
  *_strlen_recursion - Recursive function to find the length of a string
  * @s: string to find its length by recursion
- * return: Always return length
+ * return: Always 0.
  */
 
 int _strlen_recursion(char *s)
