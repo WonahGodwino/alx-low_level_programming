05354a81e269f1fbb1713150cf9d76a86ae3b9fb
WonahGodwino <atamswgo1@gmail.com>
2022-04-30 09:34:44 +0000

    Finding Empty string

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * is_palindrome - function to check for palindrome string
+ * @s: string to be checked
+ * Return: 1 or 0
+*/
+int is_palindrome(char *s)
+{
+	if (s != 'NULL')
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
