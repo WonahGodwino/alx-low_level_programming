f1bc022144590ec80876732b23be01a9e54150f9
WonahGodwino <atamswgo1@gmail.com>
2022-05-01 11:04:40 +0000

    Copy Memory address

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,11 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int j;
 
-	for (j = 0; j < n; j++)
+	while (j < n)
 	{
 		dest[j] = src[j];
+		j++
 		n--;
 	}
-	return (dest)
+	return (dest);
 }
