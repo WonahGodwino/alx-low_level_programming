e84698f40d981c6a84da9ac791d1f3d2d5ddd580
WonahGodwino <atamswgo1@gmail.com>
2022-05-01 23:38:00 +0000

    Copy memory

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,16 +10,10 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int j;
-	unsigned int i;
 
-	j = 0;
-	i = n;
-
-	while (j < i)
+	for (j = 0; j < n, j++)
 	{
 		dest[j] = src[j];
-		j++
-		i--;
 	}
 	return (dest);
 }
